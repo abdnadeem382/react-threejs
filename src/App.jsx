cc81ccdf3cc4cf0e96be73ed7138e6d4d405a773
Abdullah Nadeem <abdnadeem382@gmail.com>
2023-03-05 20:02:06 +0500

    Basic project structure done

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+import { Hero, Works, About, Contact } from "./components";
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <About />
+      <Works />
+      <Contact />
+    </Container>
+  );
+}
+
+export default App;
